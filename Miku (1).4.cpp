ca31c134e18d1cd8ee575630717575a21dcf5d56
Miku Hatsune <game.app13579@gmail.com>
2026-01-15 13:41:00 +0300

    Create Miku (1).4.cpp

---
+++ Miku (1).4.cpp
@@ -0,0 +1,28 @@
+
+
+// a) √(x1² + x2²)
+sqrt(x1*x1 + x2*x2)
+
+// b) x1 x2 + x1 x3 + x2 x3
+x1*x2 + x1*x3 + x2*x3
+
+// г) y0 t + (a t²)/2
+y0 * t + (a * t * t) / 2.0
+
+// д) √(x² + b x + c)
+sqrt(x*x + b*x + c)
+
+// е) √(a x² + b x + c)
+sqrt(a*x*x + b*x + c)
+
+// ж) 2πR
+2 * M_PI * R 
+
+// з) (a d + b c)/(a d)
+(a*d + b*c) / (a*d)
+
+// и) b² - 4 a c
+b*b - 4*a*c
+
+// к) (m1 m2)/r²
+(m1 * m2) / (r * r)
